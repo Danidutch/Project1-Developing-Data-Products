405c5159b2f045a9edd325a6737e1886a4947590
D F T <42302378+Danidutch@users.noreply.github.com>
2018-10-10 12:20:19 +0200

    mcPres.r Pubs.html

---
+++ project Shiny mcPres html.r
@@ -0,0 +1,22 @@
+# Developing-Data-Products-Final-Project
+#Tye Shiny
+#License MIT
+
+
+#Old Faithful is a cone geyser located in Yellowstone 
+#National Park in Wyoming, United States. 
+#Old Faithful was named in 1870 during the 
+#Washburn-Langford-Doane Expedition and was 
+#the first geyser in the park to receive a name.
+
+
+
+#The files of the applications are:
+##ui.R
+##server.R 
+
+
+#After a presentation of the application is disigned using 
+#slidify. The presentation could be visualize
+#https://github.com/Danidutch/Project1-Developing-Data-Products
+  
